2fc1fe93fb9675d87fca10a0a906dab47c088a67
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-09-30 16:22:39 +0900

    01_01無事完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
+template <typename T>
+T Min(T a, T b) {
+	if (a < b) {
+		return static_cast<T>(a);
+	} else if (b < a) {
+		return static_cast<T>(b);
+	} else {
+		printf("SameNumbers");
+		return static_cast<T>(a);
+	}
+}
+
+template <>
+char Min<char>(char a, char b) {
+	printf("数字以外は代入できません");
+	return a;
+}
+
 int main() {
 
-	printf("こんにちは");
+	printf("こんにちは\n");
 
+	printf("%d\n", Min<int>(10, 5));
+	printf("%f\n", Min<float>(9.0f, 12.0f));
+	printf("%lf\n", Min<double>(77.7, 55.5));
+	printf("%c\n", Min<char>(10, 5));
 
 	return 0;
 }
