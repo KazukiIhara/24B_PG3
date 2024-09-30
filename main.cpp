5da77d231874ae94d8696b5e333d54eb6436db05
IharaKazuki <kazuto3996@gmail.com>
2024-10-01 00:30:28 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <iostream>
+
+// 時給
+const int kHourlyPay = 1072;
 
 template <typename T>
 T Min(T a, T b) {
@@ -18,14 +22,33 @@ char Min<char>(char a, char b) {
 	return a;
 }
 
+// 再帰的な賃金を計算する関数
+int recursiveWage(int hour) {
+	if (hour == 1) {
+		return 100;
+	} else {
+		return recursiveWage(hour - 1) * 2 - 50;
+	}
+}
+
+// 一般的な賃金体系と再帰的な賃金体系を比較する関数
+void compareWages(int hours) {
+	int generalWage = 1072 * hours;
+	int recursiveTotalWage = 0;
+
+	for (int hour = 1; hour <= hours; ++hour) {
+		recursiveTotalWage += recursiveWage(hour);
+	}
+	std::cout << "勤務時間: " << hours << "時間" << std::endl;
+	std::cout << "一般的な賃金体系での総賃金: " << generalWage << "円" << std::endl;
+	std::cout << "再帰的な賃金体系での総賃金: " << recursiveTotalWage << "円" << std::endl;
+}
+
 int main() {
-
-	printf("こんにちは\n");
-
-	printf("%d\n", Min<int>(10, 5));
-	printf("%f\n", Min<float>(9.0f, 12.0f));
-	printf("%lf\n", Min<double>(77.7, 55.5));
-	printf("%c\n", Min<char>(10, 5));
+	int hours = 1;  // 例として8時間働く場合
+	for (hours = 0; hours <= 8; hours++) {
+		compareWages(hours);
+	}
 
 	return 0;
 }
