2fb3fffdab7925eb6ac33a135c661a9323e5d563
KazukiIhara <iharakazuki0829@gmail.com>
2024-11-13 03:02:59 +0900

    02_02無事完了

--- main.cpp
+++ main.cpp
@@ -1,15 +1,41 @@
 #include <memory>
 
-#include "Enemy.h"
+#include <iostream>
+
+template <typename Type>
+class Compare {
+public:
+    Compare(Type a, Type b) : value1(a), value2(b) {}
+
+    Type Min() const {
+        return (value1 < value2) ? value1 : value2;
+    }
+
+private:
+    Type value1;
+    Type value2;
+};
 
 int main() {
+    // int 型のインスタンス
+    Compare<int> intCompare1(3, 5);
+    Compare<int> intCompare2(10, 2);
 
-	Enemy enemy;
+    // float 型のインスタンス
+    Compare<float> floatCompare1(3.5f, 7.8f);
+    Compare<float> floatCompare2(9.1f, 1.4f);
 
-	enemy.Initialize();
+    // double 型のインスタンス
+    Compare<double> doubleCompare1(4.2, 2.3);
+    Compare<double> doubleCompare2(8.5, 10.1);
 
-	enemy.Update();
-	
-	// 終了
-	return 0;
+    // Min関数の結果を表示
+    std::cout << "intCompare1.Min(): " << intCompare1.Min() << std::endl;
+    std::cout << "intCompare2.Min(): " << intCompare2.Min() << std::endl;
+    std::cout << "floatCompare1.Min(): " << floatCompare1.Min() << std::endl;
+    std::cout << "floatCompare2.Min(): " << floatCompare2.Min() << std::endl;
+    std::cout << "doubleCompare1.Min(): " << doubleCompare1.Min() << std::endl;
+    std::cout << "doubleCompare2.Min(): " << doubleCompare2.Min() << std::endl;
+
+    return 0;
 }
